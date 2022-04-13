eb21fa921258ef82def7beea45a73cd8287bc74a
CrystalH19 <102568261+CrystalH19@users.noreply.github.com>
2022-04-12 21:08:40 -0400

    Delete genjava.js

--- javascript/genjava.js
+++
@@ -1,7 +0,0 @@
-function quizpg() {
-  document.getElementById("quizpg").onclick = window.location.href = "quiz-pg.html";
-}
-
-function quizpg() {
-  document.getElementById("dbpg").onclick = window.location.href = "database.html";
-}
