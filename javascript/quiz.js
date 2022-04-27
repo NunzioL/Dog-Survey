60fc4fc02a39f859b8cbb86856b055383d154786
CrystalH19 <102568261+CrystalH19@users.noreply.github.com>
2022-04-26 22:05:46 -0400

    Add files via upload

---
+++ javascript/quiz.js
@@ -0,0 +1,11 @@
+
+
+//loads the page to the final results page
+function final(){
+	window.location.href = "final-pg.php";
+}
+
+//loads the page back the homepage
+function exitBtn(){
+	window.location.href = "index.html";
+}
